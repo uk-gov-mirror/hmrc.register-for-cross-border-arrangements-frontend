55abbdb0bd4a8f4f29e52d9c6c0cfd119e4d9bfe
helensoniega <48728381+helensoniega@users.noreply.github.com>
2020-06-04 17:09:42 +0100

    DAC6-93 Updated regex and fixed UT

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -442,7 +442,7 @@ class NavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with Generato
                 .value
 
             navigator
-              .nextPage(ContactEmailAddressPage, NormalMode, updatedAnswers)
+              .nextPage(SecondaryContactNamePage, NormalMode, updatedAnswers)
               .mustBe(routes.ContactEmailAddressController.onPageLoad(NormalMode))
         }
       }
