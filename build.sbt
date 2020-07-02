e08df97134865a2c756212f5f4c94af508f27542
Daniel Manning <daniel-manning@users.noreply.github.com>
2020-07-02 17:37:21 +0100

    akka_override: remove akka dependence and update deprecation (#45)

--- build.sbt
+++ build.sbt
@@ -50,5 +50,3 @@ lazy val testSettings: Seq[Def.Setting[_]] = Seq(
     "-Dconfig.resource=test.application.conf"
   )
 )
-
-dependencyOverrides ++= AppDependencies.overrides
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,6 @@ object AppDependencies {
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"           % "0.18.6-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"           % "4.6.0",
     "uk.gov.hmrc"       %% "domain"                        % "5.6.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                   % "3.15.0-play-26",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.2.0-play-26",
@@ -20,26 +19,15 @@ object AppDependencies {
 
   val test = Seq(
     "org.scalatest"               %% "scalatest"             % "3.0.8",
-    "org.scalatestplus.play"      %% "scalatestplus-play"    % "4.0.0",
+    "org.scalatestplus.play"      %% "scalatestplus-play"    % "4.0.3",
     "org.pegdown"                 %  "pegdown"               % "1.6.0",
     "org.jsoup"                   %  "jsoup"                 % "1.12.1",
     "com.typesafe.play"           %% "play-test"             % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"           % "1.10.19",
     "com.github.tomakehurst"      %  "wiremock-standalone"   % "2.25.0",
-    "org.scalacheck"              %% "scalacheck"            % "1.14.0",
+    "org.scalacheck"              %% "scalacheck"            % "1.14.3",
     "wolfendale"                  %% "scalacheck-gen-regexp" % "0.1.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
-
-  val akkaVersion = "2.5.23"
-  val akkaHttpVersion = "10.0.15"
-
-  val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream"    % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf"  % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j"     % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor"     % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion
-  )
 }
